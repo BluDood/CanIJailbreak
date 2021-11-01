88456fe9317ef271034befc161e303db7410f1d7
BluDood <70075053+BluDood@users.noreply.github.com>
2021-11-01 18:10:20 +0100

    i forgot what i added, so here's your message

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,6 @@
 // ugly code ahead, bring a bag to throw up in
 
-console.warn("Only supports iPhones on iOS 14 for now!")
+console.warn("Only supports iPhone for now!")
 
 var agent = navigator.userAgent
 
@@ -8,26 +8,6 @@ var removed = agent.split(/(\s+)/).splice(0, 15);
 var version = String(removed.splice(10, 1)).replace("_", ".").replace("_", "")
 var versionnumber = Number(version)
 var versiondisplay = String(agent.split(/(\s+)/).splice(0, 15).splice(10, 1)).replace("_", ".").replace("_", ".")
-
-function isIOS() {
-    if (removed.includes('iPhone')) {
-        console.log("iPhone found!")
-    } else {
-        var first = document.getElementById("first")
-        var second = document.getElementById("second")
-        var notios = document.getElementById("notios")
-        console.log("Not iPhone")
-        first.style.display = "none";
-        second.style.display = "none";
-        notios.style.display = "block";
-    }
-}
-
-
-function iOSVersion() {
-    console.log(removed)
-    document.getElementById("version").innerHTML = versiondisplay
-}
 
 $(document).ready(function () {
     var classCycle = ['bg1', 'bg2', 'bg3', 'bg4', 'bg5', 'bg6', 'bg7', 'bg8', 'bg9', 'bg10', 'bg11', 'bg12', 'bg13', 'bg14', 'bg15',];
@@ -38,6 +18,26 @@ $(document).ready(function () {
     $('body').addClass(classToAdd);
 
 });
+
+function isIOS() {
+    if (removed.includes('iPhone')) {
+        console.debug("iPhone found!")
+    } else {
+        var first = document.getElementById("first")
+        var second = document.getElementById("second")
+        var notios = document.getElementById("notios")
+        console.debug("Not iPhone")
+        first.style.display = "none";
+        second.style.display = "none";
+        notios.style.display = "block";
+    }
+}
+
+
+function iOSVersion() {
+    console.debug(removed)
+    document.getElementById("version").innerHTML = versiondisplay
+}
 
 function Jailbreak() {
     var notice15 = document.getElementById("notice15")
