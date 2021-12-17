389e6703c062273612587ae99f5615252e641649
BluDood <70075053+BluDood@users.noreply.github.com>
2021-12-17 22:30:54 +0100

    HUGE update! � https://github.com/BluDood/CanIJailbreak/blob/main/changelogs/17Dec2021/changelog.md

--- README.md
+++ README.md
@@ -1,17 +1,16 @@
 # Can I Jailbreak?
-A website that detects your current iOS version, and says if you can jailbreak or not.
+A website that detects your current iOS/iPadOS version, and says if you can jailbreak or not.
 
-Feature list
+Features:
 - [x] Check if the device is on iOS
+- [x] Check what kind of iOS device
 - [x] Detect current iOS version
-- [ ] iPad Support
-- [x] Version-specific jailbreak tool suggestions (lazy implementation, will be more professional if needed)
-- [ ] Device-specific jailbreaks
-- [x] Suggest what to do if no jailbreak is available
-- [x] When no jailbreak is available, but an exploit is being made, show that.
-- [x] When a jailbreak is available, show the options.
-- [x] Footer with some information
+- [x] Show supported jailbreaks
+- [x] If no jailbreak is available, but an vulnerability has been found, show supported vulnerabilities
+- [x] If no jailbreaks or vulnerabilities are available, show suggestions to prepare for a jailbreak
+- [x] Show notice about iOS 15 jailbreaking
 - [x] Wiki
-- [ ] Settings, Lock Screen or Home screen theme? 👀 
 
-Repository got fucked up, but not the files themself. All commits and stuff were reset.
+TODO List:
+- Nothing at the moment!
+- If you would like something added to the site, don't hesitate to make a feature request!
--- assets/index.js
+++ assets/index.js
@@ -1,45 +1,316 @@
-// ugly code ahead, bring a bag to throw up in
+// array of current jailbreaks (if you would like to add one, make a feature request!)
+const jailbreaks = [
+    {
+        "name": "Taurine",
+        "link": "https://taurine.app",
+        "versions": [
+            "14.3",
+            "14.2.1",
+            "14.2",
+            "14.1",
+            "14.0"
+        ]
+    },
+    {
+        "name": "Odyssey",
+        "link": "https://theodyssey.dev/",
+        "versions": [
+            "13.7",
+            "13.6.1",
+            "13.6",
+            "13.5.1",
+            "13.5",
+            "13.4.1",
+            "13.4",
+            "13.3.1",
+            "13.3",
+            "13.2.3",
+            "13.2.2",
+            "13.2.1",
+            "13.2",
+            "13.1.3",
+            "13.1.2",
+            "13.1.1",
+            "13.1",
+            "13.0"
+        ]
+    },
+    {
+        "name": "Chimera",
+        "link": "https://chimera.coolstar.org/",
+        "versions": [
+            "12.5.5",
+            "12.5.4",
+            "12.5.3",
+            "12.5.2",
+            "12.5.1",
+            "12.5",
+            "12.4.9",
+            "12.4.8",
+            "12.4.7",
+            "12.4.6",
+            "12.4.5",
+            "12.4.4",
+            "12.4.3",
+            "12.4.2",
+            "12.4.1",
+            "12.4",
+            "12.3.2",
+            "12.3.1",
+            "12.3",
+            "12.2",
+            "12.1.4",
+            "12.1.3",
+            "12.1.2",
+            "12.1.1",
+            "12.1",
+            "12.0.1",
+            "12.0"
+        ]
+    },
+    {
+        "name": "Electra",
+        "link": "https://coolstar.org/electra/",
+        "versions": [
+            "11.4.1",
+            "11.4",
+            "11.3.1",
+            "11.3",
+            "11.2.6",
+            "11.2.5",
+            "11.2.2",
+            "11.2.1",
+            "11.2",
+            "11.1.2",
+            "11.1.1",
+            "11.1",
+            "11.0.3",
+            "11.0.2",
+            "11.0.1",
+            "11.0"
+        ]
+    },
+    {
+        "name": "unc0ver",
+        "link": "https://unc0ver.dev",
+        "versions": [
+            "14.5.1",
+            "14.5",
+            "14.4.2",
+            "14.4.1",
+            "14.4",
+            "14.3",
+            "14.2.1",
+            "14.2",
+            "14.1",
+            "14.0",
 
-console.warn("Only supports iPhone for now!")
+            "13.7",
+            "13.6.1",
+            "13.6",
+            "13.5.1",
+            "13.5",
+            "13.4.1",
+            "13.4",
+            "13.3.1",
+            "13.3",
+            "13.2.3",
+            "13.2.2",
+            "13.2.1",
+            "13.2",
+            "13.1.3",
+            "13.1.2",
+            "13.1.1",
+            "13.1",
+            "13.0",
 
-var agent = navigator.userAgent
+            "12.5.5",
+            "12.5.4",
+            "12.5.3",
+            "12.5.2",
+            "12.5.1",
+            "12.5",
+            "12.4.9",
+            "12.4.8",
+            "12.4.7",
+            "12.4.6",
+            "12.4.5",
+            "12.4.4",
+            "12.4.3",
+            "12.4.2",
+            "12.4.1",
+            "12.4",
+            "12.3.2",
+            "12.3.1",
+            "12.3",
+            "12.2",
+            "12.1.4",
+            "12.1.3",
+            "12.1.2",
+            "12.1.1",
+            "12.1",
+            "12.0.1",
+            "12.0",
 
-var removed = agent.split(/(\s+)/).splice(0, 15);
-var version = String(removed.splice(10, 1)).replace("_", ".").replace("_", "")
-var versionnumber = Number(version)
-var versiondisplay = String(agent.split(/(\s+)/).splice(0, 15).splice(10, 1)).replace("_", ".").replace("_", ".")
+            "11.4.1",
+            "11.4",
+            "11.3.1",
+            "11.3",
+            "11.2.6",
+            "11.2.5",
+            "11.2.2",
+            "11.2.1",
+            "11.2",
+            "11.1.2",
+            "11.1.1",
+            "11.1",
+            "11.0.3",
+            "11.0.2",
+            "11.0.1",
+            "11.0"
+        ]
+    },
+    {
+        "name": "checkra1n",
+        "link": "https://checkra.in",
+        "versions": [
+            "14.8.1",
+            "14.8",
+            "14.7.1",
+            "14.7",
+            "14.6",
+            "14.5.1",
+            "14.5",
+            "14.4.2",
+            "14.4.1",
+            "14.4",
+            "14.3",
+            "14.2.1",
+            "14.2",
+            "14.1",
+            "14.0",
 
-$(document).ready(function () {
-    var classCycle = ['bg1', 'bg2', 'bg3', 'bg4', 'bg5', 'bg6', 'bg7', 'bg8', 'bg9', 'bg10', 'bg11', 'bg12', 'bg13', 'bg14', 'bg15',];
+            "13.7",
+            "13.6.1",
+            "13.6",
+            "13.5.1",
+            "13.5",
+            "13.4.1",
+            "13.4",
+            "13.3.1",
+            "13.3",
+            "13.2.3",
+            "13.2.2",
+            "13.2.1",
+            "13.2",
+            "13.1.3",
+            "13.1.2",
+            "13.1.1",
+            "13.1",
+            "13.0",
 
-    var randomNumber = Math.floor(Math.random() * classCycle.length);
-    var classToAdd = classCycle[randomNumber];
-
-    $('body').addClass(classToAdd);
-
-});
-
-function isIOS() {
-    if (removed.includes('iPhone')) {
-        console.debug("iPhone found!")
-    } else {
-        var first = document.getElementById("first")
-        var second = document.getElementById("second")
-        var notios = document.getElementById("notios")
-        console.debug("Not iPhone")
-        first.style.display = "none";
-        second.style.display = "none";
-        notios.style.display = "block";
+            "12.5.5",
+            "12.5.4",
+            "12.5.3",
+            "12.5.2",
+            "12.5.1",
+            "12.5",
+            "12.4.9",
+            "12.4.8",
+            "12.4.7",
+            "12.4.6",
+            "12.4.5",
+            "12.4.4",
+            "12.4.3",
+            "12.4.2",
+            "12.4.1",
+            "12.4",
+            "12.3.2",
+            "12.3.1",
+            "12.3",
+            "12.2",
+            "12.1.4",
+            "12.1.3",
+            "12.1.2",
+            "12.1.1",
+            "12.1",
+            "12.0.1",
+            "12.0",
+        ],
+        "processors": [
+            "A11",
+            "A10",
+            "A9",
+            "A8",
+            "A7"
+        ]
     }
+]
+
+// array of vulnerabilities (if you would like to add one, make a feature request!)
+const vulnerabilities = [
+    {
+        "name": "CVE-2021-30955",
+        "versions": [
+            "15.1.1",
+            "15.1",
+            "15.0.2",
+            "15.0.1",
+            "15.0",
+
+            "14.8.1",
+            "14.8",
+            "14.7.1",
+            "14.7",
+            "14.6",
+            "14.5.1",
+            "14.5",
+            "14.4.2",
+            "14.4.1",
+            "14.4"
+        ],
+        "releaseDate": "~ 14. February 2022",
+        "link": "https://twitter.com/realBrightiup/status/1470535136047513609",
+        "appleSecurity": "https://support.apple.com/en-us/HT212976",
+        "processors": [
+            "All"
+        ]
+    }
+]
+
+window.jailbreakable = false
+window.vulnCompatible = false
+
+// check what the device is
+var isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
+var device = navigator.userAgent.slice(navigator.userAgent.indexOf('(') + 1, navigator.userAgent.indexOf('; CPU'))
+
+if (device == "iPhone") {
+    window.version = navigator.userAgent.slice(navigator.userAgent.indexOf('iPhone OS') + 10, navigator.userAgent.indexOf(' like Mac OS X')).replace("_", ".").replace("_", ".");
+} else if (device == "iPad") {
+    window.version = navigator.userAgent.slice(navigator.userAgent.indexOf('CPU OS') + 7, navigator.userAgent.indexOf(' like Mac OS X')).replace("_", ".").replace("_", ".");
 }
 
+if (device == "iPad") window.os = "iPadOS"; else window.os = "iOS"
 
-function iOSVersion() {
-    console.debug(removed)
-    document.getElementById("version").innerHTML = versiondisplay
-}
+if (!isIOS) {
+    console.log("This device is not on iOS!")
+    document.querySelector(".notios").style.display = "block"
+    document.querySelector(".device").style.display = "none"
+    document.querySelector(".suggestions").style.display = "none"
+} else listJailbreaks(), listVulnerabilities(), document.querySelector(".suggestions").style.display = "none"
 
-function Jailbreak() {
+// random background from selection of wallpapers (from stackoverflow, slightly edited by me so i don't need jquery)
+var classCycle = ['bg1', 'bg2', 'bg3', 'bg4', 'bg5', 'bg6', 'bg7', 'bg8', 'bg9', 'bg10', 'bg11', 'bg12', 'bg13', 'bg14', 'bg15',];
+var randomNumber = Math.floor(Math.random() * classCycle.length);
+var classToAdd = classCycle[randomNumber];
+document.body.classList.add(classToAdd)
+
+// display device and version on webpage
+document.querySelector(".device-version").innerText = `${window.os} ${window.version}`
+
+// old function for displaying jailbreaks
+function oldJailbreak() {
     var notice15 = document.getElementById("notice15")
     var ios14 = document.getElementById("ios14")
     var ios13 = document.getElementById("ios13")
@@ -47,6 +318,7 @@ function Jailbreak() {
     var vulns = document.getElementById("vulns")
     var jailbreaks = document.getElementById("jailbreaks")
     var suggestions = document.getElementById("suggestions")
+    var versionnumber = window.version
 
     if (versionnumber <= Number("14.51")) {
         status.innerHTML = "Good news, you can jailbreak! Here are some tools you can use:"
@@ -71,6 +343,52 @@ function Jailbreak() {
     }
 }
 
-function getUserAgent() {
-    document.getElementById("useragent").innerHTML = agent
+// List compatible jailbreaks
+function listJailbreaks() {
+    jailbreaks.forEach(element => {
+        var version = window.version
+        if (element.versions.includes(version)) {
+            console.log(`${element.name} is compatible with your device!`);
+            window.jailbreakable = true
+            // add to page
+            if (element.processors) var processors = `${element.processors.pop()} - ${element.processors.shift()}`; else var processors = "All"
+
+            var div = document.createElement("div")
+            div.innerHTML = `<div><h2>${element.name}</h2><h3>Supported versions:</h3><p>${element.versions.pop()} - ${element.versions.shift()}</p><h3>Supported processors:</h3><p>${processors}</p><a href="${element.link}" target="_blank">Download</a><br><br><hr></div>`
+            document.querySelector(".jailbreaks").append(div)
+
+        }
+    });
+}
+
+function listVulnerabilities() {
+    vulnerabilities.forEach(element => {
+        var version = window.version
+        if (element.versions.includes(version)) {
+            console.log(`${element.name} is compatible with your device!`);
+            window.vulnCompatible = true
+            // add to page
+            var div = document.createElement("div")
+            div.innerHTML = `<div><h2>${element.name}</h2><h3>Supported versions:</h3><p>${element.versions.pop()} - ${element.versions.shift()}</p><h3>Supported processors:</h3><p>${element.processors}</p><h3>Expected release date:</h3><p>${element.releaseDate}</p><a href="${element.link}" target="_blank">Link to Post</a><br><br><a href="${element.appleSecurity}" target="_blank">Link to Security Content</a><br><br><hr></div>`
+            document.querySelector(".jailbreaks").append(div)
+        }
+    });
+}
+
+if (!window.jailbreakable && !window.vulnCompatible) {
+    document.querySelector(".jailbreaks").style.display = "none"
+    if (isIOS) {
+        document.querySelector(".suggestions").style.display = "block"
+    }
+    document.querySelector(".status").innerText = "Sorry, but your device does not currently have any supported jailbreaks or vulnerabilities. See suggestions below:"
+} else if (window.jailbreakable && !window.vulnCompatible) {
+    document.querySelector(".status").innerText = "Good news, you can jailbreak your device! Check out these tools:"
+} else if (window.jailbreakable && window.vulnCompatible) {
+    document.querySelector(".status").innerText = "Good news, you can jailbreak your device! There are also some vulnerabilities for your version. Check out these tools and vulnerabilities:"
+} else if (!window.jailbreakable && window.vulnCompatible) {
+    document.querySelector(".status").innerText = "There are no jailbreaks available, but one or more vulnerabilities have been found:"
+}
+
+if (Number(window.version) >= 15) {
+    document.querySelector(".notice15").style.display = "block"
 }
